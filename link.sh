32ccf1e4ba14b66caa61de7e4300192273946078
Adrien <adrien.peiffer@acsone.eu>
2014-09-19 14:10:26 +0200

    [MOD]

--- link.sh
+++ link.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
  cd /home/vagrant/odoo
- sudo chown vagrant:vagrant *
- find -name "*.sh" -exec ln -s odoo/{} ../{} \; 
+ sudo -u vagrant find -name "*.sh" -exec ln -s odoo/{} ../{} \; 
  cd ..
  chmod u+x *.sh
