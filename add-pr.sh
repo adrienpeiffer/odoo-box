0d660a7c995812d2bf8c29b1ad8b1282e66cf2d9
Adrien <adrien.peiffer@acsone.eu>
2014-09-23 17:00:38 +0200

    [MOD]

--- add-pr.sh
+++ add-pr.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 AS_VAGRANT="sudo -u vagrant"
-DIR_NAME = "instance_$1"
-ADDONS_DIR_NAME = "instance_$1/$2"
+DIR_NAME = "instance-$1"
+ADDONS_DIR_NAME = "$DIR_NAME/$2"
 
 if [ $# -ne 3 ]; then
     echo "Usage : instance OCA_repository_name pull_number"
@@ -25,7 +25,6 @@ fi
 echo "Attempting merge pull request ..."
 CWD=`pwd`
 cd $ADDONS_DIR_NAME
-echo git pull --no-edit https://github.com/OCA/$2.git pull/$3/head
 git pull --no-edit https://github.com/OCA/$2.git pull/$3/head || echo "ERROR"; exit 1
 
 echo "DONE!"
