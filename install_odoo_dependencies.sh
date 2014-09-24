d1cff2378722623dbb58889b8b0767d0648f414d
Your Name <you@example.com>
2014-09-24 08:06:39 +0000

    [MOD]

--- install_odoo_dependencies.sh
+++ install_odoo_dependencies.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-# usage: ./install_odoo_dependencies.sh
+AS_VAGRANT=sudo -u vagrant
 
-$AS_VAGRANT=sudo -u vagrant
-
-if [ "x$VAGRANT_ODOO_DEPENDENCIES" == "x" ]; then
+if [ -z "$VAGRANT_ODOO_DEPENDENCIES" ]; then
 	sudo add-apt-repository -y ppa:git-core/ppa
-	sudo apt-get update
+	sudo apt-get update -y
 	sudo apt-get install -y git
 	sudo apt-get install -y python-pip
 	sudo apt-get install -y python-dev
@@ -15,8 +13,8 @@ if [ "x$VAGRANT_ODOO_DEPENDENCIES" == "x" ]; then
 	sudo apt-get install -y python-psycopg2
 	sudo apt-get install -y libpq-dev
 	sudo apt-get install -y libldap2-dev libsasl2-dev libssl-dev
-	sudo apt-get install python-virtualenv
-	sudo apt-get install bzr
+	sudo apt-get install -y python-virtualenv
+	sudo apt-get install -y bzr
 	sudo -u postgres createuser vagrant --superuser
 
 	cd /usr/local/bin
@@ -24,8 +22,7 @@ if [ "x$VAGRANT_ODOO_DEPENDENCIES" == "x" ]; then
 	sudo dpkg -i wkhtmltox-0.12.1_linux-precise-amd64.deb
 	cd /usr/bin
 	sudo ln -s ../local/bin/wkhtmltopdf wkhtmltopdf
-
-	export VAGRANT_ODOO_DEPENDENCIES='DONE'
+        echo "VAGRANT_ODOO_DEPENDENCIES='DONE'" | sudo tee -a /etc/environment
 fi
 
 echo "Ready to install Odoo !"
